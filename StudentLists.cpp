9ed1ba6787b5ce40ca69538f77db7a531f2c9e83
U-BSD\420089 <420089@SHS-B4W2NW3.beaverton.k12.or.us>
2024-10-23 15:27:53 -0700

    FINALLL COMMIT

--- StudentLists.cpp
+++ StudentLists.cpp
@@ -142,7 +142,7 @@ void addFunction(vector<Student*> & v) { // passing the vector (with the struct 
 void printFunction(vector<Student*> v) {
   for (vector<Student*>:: iterator it = v.begin(); it != v.end(); it++) {
     // create an iterator, then we will print out the all the student information when the user wishes to
-    cout << (*it) -> firstName << ", " << (*it) -> lastName << ", " << (*it) -> studentID << ", " << (*it) -> studentGPA << endl;
+    cout << (*it) -> firstName << (*it) -> lastName << ", " << (*it) -> studentID << ", " << (*it) -> studentGPA << endl;
   }
 
 
