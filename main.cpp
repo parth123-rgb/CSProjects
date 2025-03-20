7772f5f336cd2f912412052319c88ef6cc7a4a4b
parth123-rgb <420089@bsd48.org>
2025-03-20 09:37:09 -0700

    Add files via upload
    Easy.

--- main.cpp
+++ main.cpp
@@ -1,327 +1,317 @@
 #include <cstring>
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <cstdlib>
-#include <fstream>
-
 using namespace std;
 
 struct Node {
-    
-    int data;
+    char data;
+    Node* next;
     Node* right;
     Node* left;
-    
-  //we are adding a acomment here for the sake of committing changes
-  
-    
-    
-    
+
+    // newNode -> data;
 };
 
-Node* INSERTION(Node* currentNode, int input);
-bool SEARCH(Node* currentNode);
-Node* SUCCESSOR(Node* currentNode);
-
-Node* DELETION(Node* currentNode, int deleteNumber);
-void PRINT(Node* currentNode, int depth); // position is where currently am, depth is checking to see how far down the search tree to place something, size is the next open slot
-
-
+void PUSH(Node*& stackHead, Node* newNode);
+Node* POP(Node*& stackHead);
+void PRINT(Node* stackHead);
+void ENQUEUE(Node*& queueHead, Node* newNode);
+Node* DEQUEUE(Node*& queueHead);
+int PRECEDENCE(char op);
+void SHUNTINGYARD(Node*& queueHead, char infixFunction[], Node*& stackHead);
+Node* PEEK(Node* stackHead);
+void BINARYTREE(char infixFunction[], Node*& stackHead, Node*& queueHead, Node*& treeNode);
+void INFIX(Node* treeNode);
+void POSTFIX(Node* treeNode);
+void PREFIX(Node* treeNode);
 
 int main() {
-    char chooseInput[50];
-    int numberOfInput;
-    int input;
-    int rootNode;
-    bool stillGoing = true;
-    int deleteNumber;
-    char rootOrNumber[50];
-    int fileAmount;
-    int parth = 0;
-   // Node* right = new Node();
-//    Node* left = new Node();
+    Node* stackHead = NULL;
+    Node* queueHead = NULL;
     Node* right = NULL;
     Node* left = NULL;
-    Node* currentNode = NULL; // SET THE NULL SO WE DONT SEE 0
-    
-    while (stillGoing) {
-        
-    cout << "If you wish to add numbers manually, type  'ADD'" <<endl;
-    cout << "If you wish to add numbers from a file, type  'FILE'"<<endl;
-    cout << "If you wish to SEARCH for a node, type 'SEARCH'" << endl;
-    cout << "If you wish to PRINT, type 'PRINT'" << endl;
-    cout << "If you wish to delete a node, type 'DELETE'" << endl;
+    Node* treeNode = new Node();
+    Node* newNode = new Node();
+    bool whileGoing = true;
+    char input[50];
 
-cin >> chooseInput;
-  
-  
-  if (strcmp(chooseInput, "ADD")==0) { 
-    cout << "Enter the amount of numbers you wish to read in: " << endl;
-    cin >> numberOfInput;
-    
-    
-    
-    for (int i = 0; i <= numberOfInput; i++) {
-        
-        cout << "Enter which number you wish to add to tree: " << endl;
-        cin >> input;
-        INSERTION(currentNode, input);
+    char infixFunction[50];
 
-        if (input > 999 || input < 0) {
-        cout << "Your number is too high or low! "<< endl;
-        cout << "Enter a number between 1 and 999: " << endl;
-        cin >> input;
-        currentNode = INSERTION(currentNode, input); // setting  current node to the return value
+    while (whileGoing) {
+
+      cout << "Do you want to convert from INFIX to POSTFIX, PREFIX, OR CONVERTED INFIX?" << endl;
+
+      cin >> input;
+
+      cin.ignore();
+
+        Node* stackHead = NULL;
+        Node* queueHead = NULL;
+
+        if (strcmp(input, "INFIX") == 0) {
+
+	  cout << "ENTER your INFIX function: ";
+
+	  cin >> infixFunction;
+	  //	  cin.ignore();
+            SHUNTINGYARD(queueHead, infixFunction, stackHead);
+            //cout << "we get here shunting yard" << endl;
+            BINARYTREE(infixFunction, stackHead, queueHead, treeNode);
+           // cout << "we get to binary tree" << endl;
+           
+           
+            cout << "Infix Function: " << endl;
+            INFIX(treeNode);
         }
-    }
-  }
-  else if (strcmp(chooseInput, "FILE")==0) {
-        
-        cout << "Enter the amount of numbers to read in from file: " << endl;
-        cin >> fileAmount;
-        
-        ifstream fin("numbers.txt");
-        
-        for (int i = 0; i <= fileAmount; i++) {
-            
-        fin >> input;
-        cout << "Read in: " << input << endl;
-        currentNode = INSERTION(currentNode, input);
-        
-        
-            
+        else if (strcmp(input, "POSTFIX") == 0) {
+            cout << "ENTER your INFIX function: ";
+            cin >> infixFunction;
+	    //            cin.ignore();
+            SHUNTINGYARD(queueHead, infixFunction, stackHead);
+            BINARYTREE(infixFunction, stackHead, queueHead, treeNode);
+            cout << "Postfix Function: " << endl;
+            POSTFIX(treeNode);
         }
-        fin.close();
-    }
-    else if (strcmp(chooseInput, "SEARCH")==0) {
+        else if (strcmp(input, "PREFIX") == 0) {
+            cout << "ENTER your INFIX function: ";
+	    //            cin.ignore();
+            cin >> infixFunction;
+            SHUNTINGYARD(queueHead, infixFunction, stackHead);
+            BINARYTREE(infixFunction, stackHead, queueHead, treeNode);
+            cout << "Prefix Function: " << endl;
+            PREFIX(treeNode);
+        }
+
+       // cout << "POSTFIX FUNCTION: ";
         
-        SEARCH(currentNode);
-        
-    }
-    else if (strcmp(chooseInput, "PRINT")==0) {
-        
-        PRINT(currentNode, 0);
-    }
-    else if (strcmp(chooseInput, "DELETE")==0) {
-    
- //   cout << "If you want to delete the ROOT, type 'ROOT
-    cout << "Enter what number you want to delete: " << endl;
-    cin >> deleteNumber;
-    
-    currentNode = DELETION(currentNode, deleteNumber);
-    
-   // setting currnet node to the return value!
-    }
-    
-    
- }
-    
-    
-    
-    
-    
+}
     return 0;
 }
 
-
-
-Node* INSERTION(Node* currentNode, int input) {
-    
-    if (currentNode == NULL) { // empty spot, new node should be added
-        Node* temp = new Node();
-        
-        temp->data = input;
-        temp->right = NULL; // set as null, because newly inserted leafs start as a leaf (ahving no children yet)
-        temp -> left = NULL;
-        return temp;
-        
-        
-        
-        return currentNode;
-        
-    }
-    
-    
-
-    if (currentNode -> data < input) {
-        // we recursivley go down the right and left
-        // setting  the right subtree recursively with the INPUT value that the user gave us
-        currentNode -> right = INSERTION(currentNode -> right, input);
-        
-    }
-    
-    // using the value the user entered, we sort it out using the BST algorithim, recursively adding based on the user's input.
-    else if (currentNode -> data > input) {
-        currentNode -> left = INSERTION(currentNode -> left, input);
-    }
-   
-    
-    return currentNode;
-    /*
-    while (currentNode -> right != NULL) {
-    cout << "Right nodes:" << currentNode -> data;
-    }
-    
-    while (currentNode -> left != NULL) {
-        
-        cout << "Left Nodes: " << currentNode -> data;
-    }
-    
-  //  INSERTION(currentNode, input);
-    */
-}
-
-
-
-bool SEARCH(Node* currentNode) {
-    int searchInput;
-    
-    cout << "Enter the number you wish to search: " << endl;
-    cin >> searchInput;
-    
-    
-    while (currentNode != NULL) {
-        
-    
-    if (searchInput == currentNode -> data) {
-        cout << "There is: " << searchInput << " in the tree!" << endl;
-    return true; // when it is found, should return back to avoid  infinite loop
-    }
-    
-    else if (searchInput < currentNode -> data) { // comparing the input to the data values, if its less than 
-    // then we must move it, so the system can keep iterating throhg it
-        
-        currentNode = currentNode -> left; // move to this to the left, so the systme searches left
-    }
-    else if (searchInput > currentNode -> data) {
-        currentNode = currentNode->right; // same thing with right!
-        
-    
-    
-    } 
-   // else if (searchInput != currentNode->data){
-        
-       // cout << "This node is nOT in the tree" << endl;
-       // }
-        
-       // SEARCH(currentNode);
-    }
-            cout << "This node is nOT in the tree" << endl;
-            return false;
-            // when it reaches the end of the function, if it does, than it means the node is NOT in the tree.
-
-   // return currentNode;
-}
-
-
-Node* SUCCESSOR(Node* currentNode) {
-    
-    currentNode = currentNode->right;
-    while (currentNode != NULL &&  currentNode -> left != NULL) {
-    currentNode = currentNode -> left;
+void PUSH(Node*& stackHead, Node* newNode) {
+    if (newNode == NULL) {
+        return;
 
     }
-    
-    return currentNode;
 
-}
-
-
-
-
-Node* DELETION(Node* currentNode, int deleteNumber) {
-    
-    if (currentNode == NULL) {
-        return NULL;
+    if (stackHead == NULL) {
+        // if the head is open, create a new node in the head.
+        newNode->next = stackHead;
+        stackHead = newNode;
     }
-    
-    // we need to find which node to delete
-    
-    if (deleteNumber > currentNode -> data) {
-        currentNode -> right = DELETION(currentNode ->right, deleteNumber); // this is for finding the correct number to delete!
-    }
-    // checking  both right and left
-    //comparing the user's input to the data in the current node
-    // we must check both sides
-    else if (deleteNumber < currentNode -> data) {
-        currentNode -> left = DELETION(currentNode -> left, deleteNumber); 
-        // recursively finding the node on the left
-    }
+
     else {
-    //if (deleteNumber == currentNode->data) {
-        if (currentNode -> left == NULL && currentNode -> right == NULL) {
-        //Node* temp = currentNode;
-        delete currentNode;
+        newNode->next = stackHead;
+        stackHead = newNode;
+    }
+}
+
+
+
+
+Node* POP(Node*& stackHead) {
+    if (stackHead == NULL) {
         return NULL;
-        // if there is no child, then delete current node completely!
-        
+    }
+
+    Node* temp = stackHead;
+
+    stackHead = stackHead->next;
+    temp->next = NULL;
+    return temp;
+}
+
+void PRINT(Node* stackHead) {
+    Node* temp = stackHead;
+
+    while (temp != NULL) {
+        cout << temp->data;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* PEEK(Node* stackHead) {
+
+  if (stackHead == NULL) {
+        return NULL;
+    }
+    return stackHead;
+}
+
+void ENQUEUE(Node*& queueHead, Node* newNode) {
+
+  if (queueHead == NULL) {
+        queueHead = newNode;
+    }
+
+  else {
+        Node* temp = queueHead;
+
+	while (temp->next != NULL) {
+            temp = temp->next;
         }
-        else if (currentNode -> left == NULL) {
-            // if there is a right child...
-            Node* temp = currentNode->right;
-            delete currentNode; // delete currentNode
-            return temp;
-            // returning new root , we will use in main
+        temp->next = newNode;
+    }
+}
+
+Node* DEQUEUE(Node*& queueHead) {
+
+  if (queueHead == NULL) {
+        return NULL;
+    }
+
+  Node* temp = queueHead;
+    queueHead = queueHead->next;
+    temp->next = NULL;
+    return temp;
+}
+
+int PRECEDENCE(char op) {
+
+  if (op == '-' || op == '+') {
+        return 1; // least precedence, lowest on PEMDAS
+    }
+
+  else if (op == '*' || op == '/') {
+        return 2; // more precedence, but less than power
+    }
+
+  else if (op == '^') {
+        return 3; // highest precedence
+    }
+
+  else {
+        return 0; // we return 0 if the operator is not valid.
+    }
+}
+
+void SHUNTINGYARD(Node*& queueHead, char infixFunction[], Node*& stackHead) {
+      cout << "Your INFIXED function is " << infixFunction << endl;
+    int functionLength = strlen(infixFunction);
+
+    for (int i = 0; i < functionLength; i++) {
+        if (infixFunction[i] != '+' && infixFunction[i] != '-' && infixFunction[i] != '(' && infixFunction[i] != ')' && infixFunction[i] != '/' && infixFunction[i] != '^' && infixFunction[i] != '*') {
+            Node* newNode = new Node();
+            newNode->data = infixFunction[i];
+            ENQUEUE(queueHead, newNode);
         }
-        else if (currentNode -> right == NULL) { 
-        // if there is only a LEFT cchild
-        Node* temp = currentNode->left;
-        delete currentNode;
-        return temp; // returning new root
+        else if (infixFunction[i] == '+' || infixFunction[i] == '-' || infixFunction[i] == '/' || infixFunction[i] == '*' || infixFunction[i] == '^') {
+            while (stackHead != NULL && PRECEDENCE(PEEK(stackHead)->data) >= PRECEDENCE(infixFunction[i]) && (infixFunction[i] != '^' || PRECEDENCE(PEEK(stackHead)->data) > PRECEDENCE(infixFunction[i]))) {
+                Node* newNode = POP(stackHead);
+                ENQUEUE(queueHead, newNode);
+            }
+            Node* newNode = new Node();
+            newNode->data = infixFunction[i];
+            PUSH(stackHead, newNode);
         }
-        
-        else if (currentNode -> right != NULL && currentNode -> left != NULL) {
-            // if there are no empty childs (if there are two childs...)
+        else if (infixFunction[i] == '(') {
+            Node* newNode = new Node();
+            newNode->data = infixFunction[i];
+            PUSH(stackHead, newNode);
+        }
+        else if (infixFunction[i] == ')') {
+            while (stackHead != NULL && stackHead->data != '(') {
+                Node* temp = POP(stackHead);
+                ENQUEUE(queueHead, temp);
+            }
+            if (stackHead != NULL && PEEK(stackHead)->data == '(') {
+                POP(stackHead);
+            }
+        }
+    }
+
+    while (stackHead != NULL) {
+        Node* temp = POP(stackHead);
+        ENQUEUE(queueHead, temp);
+    }
+}
+
+void BINARYTREE(char infixFunction[], Node*& stackHead, Node*& queueHead, Node*& treeNode) {
+    //Node* temp = queueHead;
+    Node* numberStack = NULL;
+    
+    //    /
+        while (queueHead != NULL) {
+	 Node* newNode = DEQUEUE(queueHead);
+
+	  char infixFunction = newNode->data;
+
+            if (infixFunction != '+' && infixFunction != '-' && infixFunction != '(' && infixFunction != ')' && infixFunction != '/' && infixFunction != '^' && infixFunction != '*') {
+                
+	      newNode->left=NULL;
+		newNode->right= NULL;
+                PUSH(numberStack, newNode);
+            }
+            else {
+              //  Node* newNode = new Node();
+                //newNode->data = infixFunction[i];
+                Node* right = POP(numberStack);
+                Node* left = POP(numberStack);
+                //PUSH(numberStack, newNode);
+               // temp = DEQUEUE(queueHead);
+		  
+                        newNode->left = left;
+	    
+            newNode->right = right;
+            PUSH(numberStack, newNode);
+
+            }
             
-            Node* successorNode = SUCCESSOR(currentNode);
-            currentNode->data = successorNode->data;
-            // replace current node data with successor node data
-        currentNode->right = DELETION(currentNode->right, successorNode->data);
-
-
+            
+            //
+            ///stackHead->data;
+     //   }
+            
+            
         }
-       // return currentNode;
-//cout << "Deleted: " << currentNode;
-            // if there are two childs
-}
-return currentNode;
+        
+       treeNode = POP(numberStack);
+
 }
 
-
-/*
-void PRINT(Node* currentNode) {
-    
-    if (currentNode != NULL) {
-    cout << currentNode ->data << " "; // seperate it so it prints in order
-
-        if (currentNode -> left != NULL) { // if there are data values on the left side of the tree (if it isn't empty)
-            PRINT(currentNode->left);
-
-        }
-        if (currentNode -> right != NULL) {
-            //cout << currentNode -> data;
-            PRINT(currentNode->right); 
-        }
+void PREFIX(Node* treeNode) {
+    if (treeNode == NULL) {
+        return;
     }
-    */
-   
-   
-void PRINT(Node* currentNode, int depth) { 
-    if (currentNode == NULL) {
-        return;   // make sure the currentNode is not open
+    cout << treeNode->data;
+
+    if (treeNode->left != NULL) {
+        PREFIX(treeNode->left);
     }
 
-if (currentNode -> right != NULL) {
-    PRINT(currentNode->right, depth + 1); // if its not open, then recursivley print the right side of the tree
+    if (treeNode->right != NULL) {
+        PREFIX(treeNode->right);
+    }
 }
 
-    for (int i = 0; i < depth; i++) { // add tabs for each depth!
-        cout << "\t";  
+void POSTFIX(Node* treeNode) {
+    if (treeNode == NULL) {
+        return;
     }
-    cout << currentNode->data << endl; // print out data when going through the right side
-if (currentNode->left !=NULL) {
-    PRINT(currentNode->left, depth + 1);// do same wuth left side
+
+    if (treeNode->left != NULL) {
+        POSTFIX(treeNode->left);
+    //}
+
+   // if (treeNode->right != NULL) {
+        POSTFIX(treeNode->right);
+    }
+
+    cout << treeNode->data;
 }
-    
+
+void INFIX(Node* treeNode) {
+    if (treeNode == NULL) {
+        return;
+    }
+
+    if (treeNode->left != NULL) {
+        INFIX(treeNode->left);
+    }
+
+   cout <<  treeNode->data;
+
+    if (treeNode->right != NULL) {
+        INFIX(treeNode->right);
+    }
 }
